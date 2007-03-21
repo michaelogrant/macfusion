6ca9dcf5eef938c6e236d594323c218cdc0a8584
mgorbach <mgorbach@a94ec7ac-d928-0410-bd00-8782369af1f7>
2007-03-21 04:11:23 +0000

    The beginnings of FTP ... I present to the world a statically linked, dynamic curlftpfs binary! (It took forever to get this binary created properly, please check that it runs on your system)

---
+++ FTPFS/FTPFSUIController.m
@@ -0,0 +1,14 @@
+//
+//  FTPFSUIController.m
+//  MacFusion
+//
+//  Created by Michael Gorbach on 3/19/07.
+//  Copyright 2007 Michael Gorbach. All rights reserved.
+//
+
+#import "FTPFSUIController.h"
+
+
+@implementation FTPFSUIController
+
+@end
